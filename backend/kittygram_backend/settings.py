531a2185ab5ffbb1755c73a528f7745281918619
Alexander Chernovasilenko <a.chernovasilenko@yandex.ru>
2024-03-16 20:39:30 +0500

    add static

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -12,7 +12,8 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['84.201.176.117', '127.0.0.1', 'localhost', 'kottygram.zapto.org']
+ALLOWED_HOSTS = ['84.201.176.117', '127.0.0.1',
+                 'localhost', 'kottygram.zapto.org']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -96,6 +97,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
