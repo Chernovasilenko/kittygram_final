ca5b129b11b52dd702a9cdcbd180ac3f01a7e41b
Chernovasilenko <a.chernovasilenko@yandex.ru>
2024-03-18 14:46:36 +0500

    fix rev

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,13 +8,9 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-#SECRET_KEY = os.environ['SECRET_KEY']
 SECRET_KEY = os.getenv('SECRET_KEY', 'KEY')
 
-#DEBUG = os.environ['DEBUG']
 DEBUG = os.getenv('DEBUG', 'False')
-
-#ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(';')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1;localhost').split(';')
 
